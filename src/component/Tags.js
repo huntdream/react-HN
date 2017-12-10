81e8f5e243209d64d913f28fa5104a326b2ba7f5
Yu <maoyu960320@hotmail.com>
2017-12-10 14:58:52 +0800

    css adjustment

--- src/component/Tags.js
+++ src/component/Tags.js
@@ -1,15 +1,16 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 
 class Tags extends Component {
-    render(){
+    render() {
         const tags = this.props.tags;
-        return(
-            <div className="col-md-4 order-first justify-content-center row align-items-start" onClick={this.props.onClick}>
+        return (
+            <div className="col-md-4 order-first justify-content-center row align-items-start"
+                 onClick={this.props.onClick}>
                 {
-                    tags.map(tag=>
-                    <button className={"btn btn-outline-secondary m-1"} key={tag}>
-                        {tag.toUpperCase()}
-                    </button>
+                    tags.map(tag =>
+                        <button className={"btn btn-outline-secondary m-1"} key={tag}>
+                            {tag.toUpperCase()}
+                        </button>
                     )
                 }
             </div>
